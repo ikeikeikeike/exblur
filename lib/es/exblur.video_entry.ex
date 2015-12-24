dc85d4287399def98a8adac601a638c88bc60bbb
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-12-24 11:22:19 +0900

    tiny fix

--- lib/es/exblur.video_entry.ex
+++ lib/es/exblur.video_entry.ex
@@ -15,13 +15,13 @@ defmodule Es.Exblur.VideoEntry do
       Tirexs.DSL.define [type: "dsl", index: @index_name], fn(index, es_settings) ->
         settings do
           analysis do
-            filter    "exblur_posfilter", type: "kuromoji_part_of_speech", stoptags: ["助詞-格助詞-一般", "助詞-終助詞"]
+            filter    "ja_posfilter", type: "kuromoji_part_of_speech", stoptags: ["助詞-格助詞-一般", "助詞-終助詞"]
 
-            tokenizer "exblur_tokenizer", type: "kuromoji_tokenizer"
+            tokenizer "ja_tokenizer", type: "kuromoji_tokenizer"
             tokenizer "ngram_tokenizer",  type: "nGram", min_gram: "2", max_gram: "3", token_chars: ["letter", "digit"]
 
-            analyzer  "default",          type: "custom", tokenizer: "exblur_tokenizer"
-            analyzer  "exblur_analyzer",  type: "custom", tokenizer: "exblur_tokenizer", filter: ["kuromoji_baseform", "exblur_posfilter", "cjk_width"]
+            analyzer  "default",          type: "custom", tokenizer: "ja_tokenizer"
+            analyzer  "ja_analyzer",  type: "custom", tokenizer: "ja_tokenizer", filter: ["kuromoji_baseform", "ja_posfilter", "cjk_width"]
             analyzer  "ngram_analyzer",                   tokenizer: "ngram_tokenizer"
           end
         end
@@ -41,8 +41,8 @@ defmodule Es.Exblur.VideoEntry do
           indexes "tags",           type: "string", index: "not_analyzed"
           indexes "divas",          type: "string", index: "not_analyzed"
 
-          indexes "title",          type: "string", analyzer: "exblur_analyzer"
-          indexes "content",        type: "string", analyzer: "exblur_analyzer"
+          indexes "title",          type: "string", analyzer: "ja_analyzer"
+          indexes "content",        type: "string", analyzer: "ja_analyzer"
 
           indexes "time",           type: "long"
           indexes "published_at",   type: "date",   format: "dateOptionalTime"
