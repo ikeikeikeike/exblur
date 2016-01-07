055983aa78f42894aea70677a4da70e906597572
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2016-01-07 19:33:47 +0900

    view and template

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Exblur.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/entries", EntryController, :index
   end
 
   # Other scopes may use custom stacks.
---
+++ web/views/entry_view.ex
@@ -0,0 +1,3 @@
+defmodule Exblur.EntryView do
+  use Exblur.Web, :view
+end
