0726ecc815d36a78732b99e2fec888a8d22f05b2
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-02-12 04:18:17 +0900

    fix proofreading module

--- lib/translator/proofreading.ex
+++ lib/translator/proofreading.ex
@@ -1,26 +1,47 @@
 defmodule Translator.Proofreading do
+  alias Translator.Proofreading, as: Proof
 
-      # sentence needs data types below.
-      #
-      #  - from_word: 'from word'
-      #  - to_word: 'to_word'
-      #  or
-      #  - from_word: 'from word'
-      #  - to_word: 'word1,word2,ward3'
-      #
-      def sentence(word) when "" ==  word,  do: word
-      def sentence(word) when is_nil(word), do: word
-      def sentence(word) do
+  def configure(tags_filter \\ [], sentences_filter \\ []) do
+    start_link([
+      tags: tags_filter,  #|| Filter.find_tags.as_filter,
+      sentences: sentences_filter  #|| Filter.find_sentences.as_filter
+    ])
+    {:ok, []}
+  end
 
-        # Enum.each @filters[:tags], fn{key, value} ->
-          # word = word.sub(key, value)
-        # end
+  defp start_link(value) do
+    Agent.start_link(fn -> value end, name: __MODULE__)
+  end
 
-        # Enum.each @filters[:sentences], fn(key, values) ->
-          # word = word.sub(key, values.split(',').sample)
-        # end
+  @doc """
+  Get the filters
+  """
+  def filters do
+    Agent.get(__MODULE__, fn config -> config end)
+  end
 
-        word
-      end
+  # sentence needs data types below.
+  #
+  #  - from_word: 'from word'
+  #  - to_word: 'to_word'
+  #  or
+  #  - from_word: 'from word'
+  #  - to_word: 'word1,word2,ward3'
+  #
+  def sentence(word) when "" ==  word,  do: word
+  def sentence(word) when is_nil(word), do: word
+  def sentence(word) do
+    filters = Proof.filters
+
+    Enum.each filters[:tags], fn{key, value} ->
+      word = String.replace(word, key, value)
+    end
+
+    Enum.each filters[:sentences], fn(key, values) ->
+      word = String.replace(word, key, Enum.random(String.split(values, ",")))
+    end
+
+    word
+  end
 
 end
