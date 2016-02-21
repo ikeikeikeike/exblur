ac5fe194de42c6370137803ea8ebb0651a17b28d
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-02-22 03:12:15 +0900

    make template in progress

--- lib/device_detector.ex
+++ lib/device_detector.ex
@@ -2,6 +2,12 @@ defmodule DeviceDetector do
   alias UAInspector.Result
   alias UAInspector.Parser
 
+  def desctop?(%Plug.Conn{} = conn) do
+    conn
+    |> Plug.Conn.get_req_header("user-agent")
+    |> List.first
+    |> desctop?
+  end
   def desctop?(useragent) do
     case Parser.parse(useragent) do
       %Result{device: %Result.Device{type: "smartphone"}} -> false
@@ -17,11 +23,18 @@ defmodule DeviceDetector do
     end
   end
 
+  def mobile?(%Plug.Conn{} = conn) do
+    conn
+    |> Plug.Conn.get_req_header("user-agent")
+    |> List.first
+    |> mobile?
+  end
   def mobile?(useragent) do
     case Parser.parse(useragent) do
       %Result{device: %Result.Device{type: "desktop"}} -> false
       %Result{device: %Result.Device{type: "console"}} -> false
       %Result{device: %Result.Device{type: "tv"}} -> false
+      %Result{device: %Result.Device{type: :unknown, brand: :unknown, model: :unknown}} -> false
       %Result.Bot{user_agent: ua} -> Regex.match?(~r/mobile/i, ua)
       _ -> true
     end
