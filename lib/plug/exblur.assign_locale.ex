43174ad5c79d8b361c3ea481998d8f4dfe9248bc
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-02-28 19:33:31 +0900

    added plug for locale

---
+++ lib/plug/exblur.assign_locale.ex
@@ -0,0 +1,41 @@
+defmodule Plug.Exblur.AssignLocale do
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    language_tag = List.first(extract_accept_language(conn))
+    if language_tag do
+      locale = Exblur.Gettext.find_locale(language_tag) ||
+        Exblur.Gettext.default_locale
+      Plug.Conn.assign(conn, :locale, locale)
+    else
+      Plug.Conn.assign(conn, :locale, Exblur.Gettext.default_locale)
+    end
+  end
+
+  # Adapted from http://code.parent.co/practical-i18n-with-phoenix-and-elixir/
+  defp extract_accept_language(conn) do
+    case conn |> Plug.Conn.get_req_header("accept-language") do
+      [value|_] ->
+        value
+        |> String.replace(" ", "")
+        |> String.split(",")
+        |> Enum.map(&parse_language_option/1)
+        |> Enum.sort(&(&1.quality >= &2.quality))
+        |> Enum.map(&(&1.tag))
+      _ ->
+        []
+    end
+  end
+
+  defp parse_language_option(string) do
+    captures = ~r/^(?<tag>[\w\-]+)(?:;q=(?<quality>[\d\.]+))?$/i
+    |> Regex.named_captures(string)
+
+    quality = case Float.parse(captures["quality"] || "1.0") do
+      {val, _} -> val
+      _ -> 1.0
+    end
+
+    %{tag: captures["tag"], quality: quality}
+  end
+end
---
+++ lib/plug/exblur.configure_gettext.ex
@@ -0,0 +1,15 @@
+defmodule Plug.Exblur.ConfigureGettext do
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    locale = conn.assigns[:locale]
+    if locale in Gettext.known_locales(Exblur.Gettext) do
+      Gettext.put_locale(Exblur.Gettext, locale)
+      Gettext.put_locale(Phoenix.HTML.SimplifiedHelpers.Gettext, locale)
+      conn
+    else
+      conn
+    end
+  end
+end
