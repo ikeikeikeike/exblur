ef6fba2d15cf50909afc8935a9dc20b411f2d948
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-03-26 19:12:48 +0900

    todo

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Exblur.Mixfile do
   end
 
   defp version do
-    v = "0.2.15"
+    v = "0.2.16"
     File.write! "VERSION", v
     v
   end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,4 +3,4 @@ ExUnit.start
 Mix.Task.run "ecto.create", ["--quiet"]
 Mix.Task.run "ecto.migrate", ["--quiet"]
 Ecto.Adapters.SQL.begin_test_transaction(Exblur.Repo)
-
+Ecto.Adapters.SQL.begin_test_transaction(Exblur.Mongo)
