5e5d683ba9b588bde2b7d2a5f97c04f9c3750539
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-04-09 23:59:44 +0900

    remove converter

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Exblur.Mixfile do
   end
 
   defp version do
-    v = "0.3.29"
+    v = "0.3.34"
     File.write! "VERSION", v
     v
   end
--- web/uploaders/thumb_uploader.ex
+++ web/uploaders/thumb_uploader.ex
@@ -2,7 +2,7 @@ defmodule Exblur.ThumbUploader do
   use Arc.Definition
   use Arc.Ecto.Definition
 
-  @versions [:original, :size_100x66, :size_200x132, :size_300x200]
+  @versions [:original]
   @extension_whitelist ~w(.jpg .jpeg .gif .png .ico .bmp)
 
   def acl(:thumb, _), do: :public_read
@@ -12,17 +12,9 @@ defmodule Exblur.ThumbUploader do
     Enum.member?(@extension_whitelist, file_extension)
   end
 
-  def transform(:size_100x66, _) do
-    {:convert, "-thumbnail 100x66^ -gravity center -extent 100x66"}
-  end
-
-  def transform(:size_200x132, _) do
-    {:convert, "-thumbnail 200x132^ -gravity center -extent 200x132"}
-  end
-
-  def transform(:size_300x200, _) do
-    {:convert, "-thumbnail 300x200^ -gravity center -extent 300x200"}
-  end
+  # def transform(:size_100x66, _) do
+    # {:convert, "-thumbnail 100x66^ -gravity center -extent 100x66"}
+  # end
 
   def s3_object_headers(_version, {file, _scope}) do
     [content_type: Plug.MIME.path(file.file_name)] # for "image.png", would produce: "image/png"
@@ -47,18 +39,6 @@ defmodule Exblur.ThumbUploader do
   end
 
   def default_url(:original) do
-    "https://placehold.it/300x200"
-  end
-
-  def default_url(:size_100x66) do
-    "https://placehold.it/100x66"
-  end
-
-  def default_url(:size_200x132) do
-    "https://placehold.it/200x132"
-  end
-
-  def default_url(:size_300x200) do
     "https://placehold.it/300x200"
   end
 
