4c94715bc91ec16c76dd5a3d9519d3fa8f946ee7
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-04-17 21:58:43 +0900

    date of birth

--- web/controllers/diva/birthday_controller.ex
+++ web/controllers/diva/birthday_controller.ex
@@ -6,7 +6,34 @@ defmodule Exblur.Diva.BirthdayController do
   require Tirexs.Query
 
   def month(conn, %{"year" => year, "month" => month}) do
-    render(conn, "month.html")
+    {year, _} = Integer.parse(year)
+    {month, _} = Integer.parse(month)
+    next_month =
+      Timex.Date.from({year, month, 01})
+      |> Timex.Date.shift(months: 1)
+
+    birthdays =
+      Model
+      |> select([p], p.birthday)
+      |> where([q], q.appeared > 0)
+      |> where([q], not is_nil(q.birthday))
+      |> where([q], q.birthday  < ^next_month)
+      |> where([q], q.birthday >= ^Timex.Date.from({year, month , 01}))
+      |> order_by([q], [asc: q.birthday])
+      |> Exblur.Repo.all
+      |> Enum.uniq_by(fn birthday ->
+        if birthday, do: birthday.day
+      end)
+
+   divas =
+      Model
+      |> where([q], q.appeared > 0)
+      |> where([q], not is_nil(q.birthday))
+      |> where([q], q.birthday  < ^next_month)
+      |> where([q], q.birthday >= ^Timex.Date.from({year, month , 01}))
+      |> Exblur.Repo.all
+
+    render(conn, "month.html", birthdays: birthdays, divas: divas)
   end
 
   def year(conn, %{"year" => year}) do
@@ -23,7 +50,15 @@ defmodule Exblur.Diva.BirthdayController do
         if birthday, do: birthday.month
       end)
 
-    render(conn, "year.html", birthdays: birthdays)
+   divas =
+      Model
+      |> where([q], q.appeared > 0)
+      |> where([q], not is_nil(q.birthday))
+      |> where([q], q.birthday <= ^"#{year}-12-31")
+      |> where([q], q.birthday >= ^"#{year}-01-01")
+      |> Exblur.Repo.all
+
+    render(conn, "year.html", birthdays: birthdays, divas: divas)
   end
 
   def index(conn, _params) do
