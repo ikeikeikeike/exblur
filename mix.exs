5cdc27a6f003bb566fe08f38146905a26d35ec90
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-04-18 22:28:39 +0900

    for seo

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Exblur.Mixfile do
   end
 
   defp version do
-    v = "0.4.12"
+    v = "0.4.13"
     File.write! "VERSION", v
     v
   end
--- web/views/diva_view.ex
+++ web/views/diva_view.ex
@@ -10,7 +10,7 @@ defmodule Exblur.DivaView do
     %{value: diva.name, tokens: String.split(diva.name)}
   end
 
-  def page_title(_, _), do: gettext("All Divas") <> " - " <> gettext("diva page title with %{name}", name: gettext("Site Name"))
+  def page_title(_, _), do: gettext("All Divas") <> " - " <> gettext("Default Page Title")
   def page_keywords(_, _), do: gettext("Default,Page,Keywords") <> "," <> gettext("Diva,Page,Keywords")
   def page_description(_, _), do: gettext "diva page title with %{name}", name: gettext("Site Name")
 
