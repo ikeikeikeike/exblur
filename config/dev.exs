c08c674d2bffc8be0fef203044749ad1fe5ab444
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-05-10 22:49:06 +0900

    diva link

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :exblur, Exblur.Endpoint,
 config :exblur, Exblur.Endpoint,
   live_reload: [
     patterns: [
-      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      # ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/models/.*(ex)$},
       ~r{web/controllers/.*(ex)$},
