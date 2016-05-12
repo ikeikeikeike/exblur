fc5ef904a4b62ceb43819ab028e0b33b8faa00c6
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-05-12 15:42:55 +0900

    model

---
+++ wikiprof/models.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy.ext.declarative.api import declarative_base
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import (
+    DateTime,
+    Integer,
+    String
+)
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+)
+
+Base = declarative_base()
+DBSession = scoped_session(sessionmaker())
+
+
+class Diva(Base):
+    __tablename__ = 'divas'
+    query = DBSession.query_property()
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    kana = Column(String(255))
+    romaji = Column(String(255))
+    gyou = Column(String(255))
+
+    height = Column(Integer)
+    weight = Column(Integer)
+
+    bust = Column(Integer)
+    bracup = Column(String(255))
+    waste = Column(Integer)
+    hip = Column(Integer)
+
+    blood = Column(String(255))
+    birthday = Column(DateTime)
