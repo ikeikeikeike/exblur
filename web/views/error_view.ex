69674a575dd687f21196f835a6adb648fc455860
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-08-25 17:26:24 +0900

    router

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,14 +1,6 @@
 defmodule Exblur.ErrorView do
   use Exblur.Web, :view
 
-  def render("404.html", assigns) do
-    render "404.html", assigns
-  end
-
-  def render("500.html", assigns) do
-    render "500.html", assigns
-  end
-
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
