b405ac9e1567ea903d80a52663ce16aa23266b19
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-01-03 18:09:25 +0900

    bugfix

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Exblur.Mixfile do
   end
 
   defp version do
-    v = "0.10.5"
+    v = "0.10.6"
     File.write! "VERSION", v
     v
   end
--- web/controllers/about_controller.ex
+++ web/controllers/about_controller.ex
@@ -1,6 +1,9 @@
 defmodule Exblur.AboutController do
   use Exblur.Web, :controller
 
+  plug Exblur.Ctrl.Plug.AssignTag
+  plug Exblur.Ctrl.Plug.AssignDiva
+
   def index(conn, _params) do
     render conn, "index.html"
   end
